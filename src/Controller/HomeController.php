a84d797083066175a91cf7df634062fb61cd4684
chikungugna <thomas.rey@posteo.net>
2019-05-25 14:22:05 +0200

    racine homepage ok

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+
+class HomeController extends AbstractController
+{
+	/**
+	 * @Route("/", name="homepage")
+	 */
+	public function index() {
+		return $this->render('home.html.twig');
+	}
+}
