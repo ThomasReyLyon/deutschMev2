39df8266bea681730acca778dfc7df15ddb0068a
chikungugna <thomas.rey@posteo.net>
2019-05-27 09:21:34 +0200

    ajout x ou y

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -24,7 +24,7 @@ class Category
     private $name;
 
     /**
-     * @ORM\OneToMany(targetEntity="App\Entity\Article", mappedBy="Category")
+     * @ORM\OneToMany(targetEntity="App\Entity\Article", mappedBy="category")
      */
     private $articles;
 
--- src/Migrations/Version20190525125302.php
+++ src/Migrations/Version20190525125302.php
@@ -22,11 +22,11 @@ final class Version20190525125302 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article ADD category_id INT NOT NULL, ADD author_id INT NOT NULL');
-        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
-        $this->addSql('ALTER TABLE article ADD CONSTRAINT FK_23A0E66F675F31B FOREIGN KEY (author_id) REFERENCES author (id)');
-        $this->addSql('CREATE INDEX IDX_23A0E6612469DE2 ON article (category_id)');
-        $this->addSql('CREATE INDEX IDX_23A0E66F675F31B ON article (author_id)');
+        $this->addSql('ALTER TABLE articles ADD category_id INT NOT NULL, ADD author_id INT NOT NULL');
+        $this->addSql('ALTER TABLE articles ADD CONSTRAINT FK_23A0E6612469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE articles ADD CONSTRAINT FK_23A0E66F675F31B FOREIGN KEY (author_id) REFERENCES author (id)');
+        $this->addSql('CREATE INDEX IDX_23A0E6612469DE2 ON articles (category_id)');
+        $this->addSql('CREATE INDEX IDX_23A0E66F675F31B ON articles (author_id)');
     }
 
     public function down(Schema $schema) : void
@@ -34,10 +34,10 @@ final class Version20190525125302 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E6612469DE2');
-        $this->addSql('ALTER TABLE article DROP FOREIGN KEY FK_23A0E66F675F31B');
-        $this->addSql('DROP INDEX IDX_23A0E6612469DE2 ON article');
-        $this->addSql('DROP INDEX IDX_23A0E66F675F31B ON article');
-        $this->addSql('ALTER TABLE article DROP category_id, DROP author_id');
+        $this->addSql('ALTER TABLE articles DROP FOREIGN KEY FK_23A0E6612469DE2');
+        $this->addSql('ALTER TABLE articles DROP FOREIGN KEY FK_23A0E66F675F31B');
+        $this->addSql('DROP INDEX IDX_23A0E6612469DE2 ON articles');
+        $this->addSql('DROP INDEX IDX_23A0E66F675F31B ON articles');
+        $this->addSql('ALTER TABLE articles DROP category_id, DROP author_id');
     }
 }
